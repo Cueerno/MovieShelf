a3669a7f509492c72753202d4b4232948dcc280a
Vyacheslav Radiuk <vyacheslavradiuk@gmail.com>
2025-07-09 00:50:03 +0300

    refactor: improve methods names

--- backend/services/movie-shelf-backend-core/src/main/java/com/radiuk/movieshelfbackendcore/controller/MovieController.java
+++ backend/services/movie-shelf-backend-core/src/main/java/com/radiuk/movieshelfbackendcore/controller/MovieController.java
@@ -4,6 +4,8 @@ package com.radiuk.movieshelfbackendcore.controller;
 import com.radiuk.movieshelfbackendcore.service.MovieService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -16,12 +18,12 @@ public class MovieController {
     private final MovieService movieService;
 
     @GetMapping("/search")
-    public ResponseEntity<?> searchMovies(@RequestParam String query) {
-        return ResponseEntity.ok(movieService.searchMovies(query));
+    public ResponseEntity<?> searchByTitle(@RequestParam String query) {
+        return ResponseEntity.ok(movieService.searchByTitle(query));
     }
 
     @GetMapping("/{imdbId}")
-    public ResponseEntity<?> getMovie(@PathVariable String imdbId) {
-        return ResponseEntity.ok(movieService.getMovieByImdbId(imdbId));
+    public ResponseEntity<?> getByImbdId(@PathVariable String imdbId, @AuthenticationPrincipal UserDetails userDetails) {
+        return ResponseEntity.ok(movieService.findByImdbId(imdbId, userDetails.getUsername()));
     }
 }
