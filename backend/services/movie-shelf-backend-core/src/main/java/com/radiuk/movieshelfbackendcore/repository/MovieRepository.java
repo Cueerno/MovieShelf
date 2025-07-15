31d70da7bc16d412c7d2ec65e360e6f3459c845d
Vyacheslav Radiuk <vyacheslavradiuk@gmail.com>
2025-07-15 23:36:55 +0300

    perf: add solve n+1 problem in MovieRepository

--- backend/services/movie-shelf-backend-core/src/main/java/com/radiuk/movieshelfbackendcore/repository/MovieRepository.java
+++ backend/services/movie-shelf-backend-core/src/main/java/com/radiuk/movieshelfbackendcore/repository/MovieRepository.java
@@ -14,6 +14,7 @@ import java.util.Optional;
 @Repository
 public interface MovieRepository extends JpaRepository<Movie, Long> {
 
+    @EntityGraph(attributePaths = "ratings")
     Optional<Movie> findByImdbId(String imdbId);
 
     @EntityGraph(attributePaths = "ratings")
