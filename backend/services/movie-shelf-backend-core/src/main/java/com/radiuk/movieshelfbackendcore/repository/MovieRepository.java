3bffee4dade3c9c655c5f6bfd3c56eacf70c081f
Vyacheslav Radiuk <vyacheslavradiuk@gmail.com>
2025-07-18 13:07:22 +0300

    perf: improve query that retrieving data about movie
    - 5x subqueries -> 5x joins

--- backend/services/movie-shelf-backend-core/src/main/java/com/radiuk/movieshelfbackendcore/repository/MovieRepository.java
+++ backend/services/movie-shelf-backend-core/src/main/java/com/radiuk/movieshelfbackendcore/repository/MovieRepository.java
@@ -36,14 +36,20 @@ public interface MovieRepository extends JpaRepository<Movie, Long> {
 
     @Query(value = """
     select
-        (select exists(select 1 from favorites where movie_id = m.id and user_id = :userId)) is_favorite,
-        (select count(*) from favorites where movie_id = m.id) favorites_count,
-        (select cast(avg(score) as smallint) from rating where movie_id = m.id) average_rating,
-        (select count(*) from comment where movie_id = m.id) comments_count,
-        (select count(*) from reaction where movie_id = m.id and like_type = 'LIKE') likes_count,
-        (select count(*) from reaction where movie_id = m.id and like_type = 'DISLIKE') dislikes_count
+        exists(select 1 from favorites f where f.movie_id = m.id and f.user_id = :userId) is_favorite,
+        count(distinct f.user_id) favorite_count,
+        cast(avg(r.score) as smallint) average_rating,
+        count(distinct c.id) comment_count,
+        count(distinct rl.user_id) likes_count,
+        count(distinct rd.user_id) dislikes_count
     from movie m
+    left join favorites f on f.movie_id = m.id
+    left join rating r on r.movie_id = m.id
+    left join comment c on c.movie_id = m.id
+    left join reaction rl on rl.movie_id = m.id and rl.like_type = 'LIKE'
+    left join reaction rd on rd.movie_id = m.id and rd.like_type = 'DISLIKE'
     where m.imdb_id = :imdbId
+    group by m.id;
     """, nativeQuery = true)
     AdditionalMovieInformation findAdditionalMovieInformationByMovieImdbId(String imdbId, Long userId);
 }
--- backend/services/movie-shelf-backend-core/src/main/java/com/radiuk/movieshelfbackendcore/service/UserDetailsServiceImpl.java
+++ backend/services/movie-shelf-backend-core/src/main/java/com/radiuk/movieshelfbackendcore/service/UserDetailsServiceImpl.java
@@ -18,6 +18,7 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        System.out.println("UserDetailsServiceImpl loadUserByUsername " + username);
         return new UserDetailsImpl(user);
     }
 }
